2232164cef8411059f2523f783c3127602f8b554
Sangjun Son <sangjun.son@moloco.com>
2022-11-06 15:36:36 +0900

    Make as workflow dispatch

--- tools/test.sh
+++ tools/test.sh
@@ -7,22 +7,4 @@
 #   echo "No"
 # fi
 
-
-PUBLIC_KEY_JSON=$(gh api \
-  -H "Accept: application/vnd.github+json" \
-  /repos/lucetre/beautify-github-contribution/actions/secrets/public-key)
-
-PUBLIC_KEY_ID=$(jq '.key_id' <<<$PUBLIC_KEY_JSON | tr -d '"')
-PUBLIC_KEY=$(jq '.key' <<<$PUBLIC_KEY_JSON)
-ENCRYPTED_SECRET=`python3 tools/encrypt_github_secret.py $PUBLIC_KEY "hello"`
-
-gh api \
-  --method PUT \
-  -H "Accept: application/vnd.github+json" \
-  /repos/lucetre/beautify-github-contribution/actions/secrets/TEST_SECRET \
-  -f encrypted_value="$ENCRYPTED_SECRET" \
-  -f key_id=$PUBLIC_KEY_ID | jq
-
-gh api \
-  -H "Accept: application/vnd.github+json" \
-  /repos/lucetre/beautify-github-contribution/actions/secrets | jq
+gh workflow run register-github-secrets.yaml -f SECRET_NAME=scully -f SECRET_VALUE=hello
