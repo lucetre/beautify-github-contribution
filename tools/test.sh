570bab23957df3e00398620db3bac6e6057db6fb
Sangjun Son <sangjun.son@moloco.com>
2022-11-06 16:59:38 +0900

    Introduce NEW_ISSUE_BODY

--- tools/test.sh
+++ tools/test.sh
@@ -7,4 +7,11 @@
 #   echo "No"
 # fi
 
-gh workflow run register-github-secrets.yaml -f SECRET_NAME=scully -f SECRET_VALUE=hello
+# gh workflow run register-github-secrets.yaml -f SECRET_NAME=scully -f SECRET_VALUE=hello
+
+text='''
+fdsafs\nPATTERN-1\nFirst line of unimportant text\nSecond line of unimportant text\nPATTERN-2\nSome more texts (may/ maynt be important!)\ndskfl\nfds
+'''
+new_text=$(echo -e "$text" | sed '/PATTERN-1/,/PATTERN-2/d')
+echo -e "$text"
+echo "$new_text"
