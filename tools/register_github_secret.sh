1ce26ee0714e4dfb750d52c6397f48dc7c04ad85
Sangjun Son <lucetre@naver.com>
2022-11-06 15:09:15 +0900

    Feature/register GitHub secrets (#37)
    * Experiment list repo secrets
    
    * Change secret to pat
    
    * Get repo public key
    
    * Show test secret
    
    * Update secret via github action
    
    * Check if encrypting works well
    
    * When are the secrets being rendered
    
    * Show updated secret
    
    * Hi mom
    
    * Hi dad
    
    * Hello dad
    
    * Check if encrypting works well
    
    * GitHub secret render before execution
    
    * New issue template
    
    * New issue template

---
+++ tools/register_github_secret.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# The script encrypts the custom secret value and uploads it to the current GitHub repository.
+#
+# Usage:
+# bash tools/register_github_secret.sh
+#   $SECRET_NAME: name of the repository secret to create or update
+#   $SECRET_VALUE: value of the repository secret
+#
+# Related task: (@lucetre)
+
+pip install pynacl
+
+PUBLIC_KEY_JSON=$(gh api \
+  -H "Accept: application/vnd.github+json" \
+  /repos/lucetre/beautify-github-contribution/actions/secrets/public-key)
+
+PUBLIC_KEY_ID=$(jq -r '.key_id' <<<$PUBLIC_KEY_JSON)
+PUBLIC_KEY=$(jq -r '.key' <<<$PUBLIC_KEY_JSON)
+ENCRYPTED_SECRET=$(python tools/encrypt_github_secret.py $PUBLIC_KEY "$SECRET_VALUE")
+echo $PUBLIC_KEY_ID $PUBLIC_KEY $ENCRYPTED_SECRET
+
+gh api \
+  --method PUT \
+  -H "Accept: application/vnd.github+json" \
+  /repos/lucetre/beautify-github-contribution/actions/secrets/$SECRET_NAME \
+  -f encrypted_value="$ENCRYPTED_SECRET" \
+  -f key_id=$PUBLIC_KEY_ID | jq
+
+gh api \
+  -H "Accept: application/vnd.github+json" \
+  /repos/lucetre/beautify-github-contribution/actions/secrets | jq
--- tools/test.sh
+++ tools/test.sh
@@ -1,8 +1,28 @@
-CONTRIBUTOR=linkedart-snu
-str=$(cat "scheduler/2022/10/30/CONTRIBUTORS" | grep $CONTRIBUTOR | tr -d '[:space:]')
+# CONTRIBUTOR=linkedart-snu
+# str=$(cat "scheduler/2022/10/30/CONTRIBUTORS" | grep $CONTRIBUTOR | tr -d '[:space:]')
 
-if [[ $str == $CONTRIBUTOR ]]; then
-  echo "Trigger"
-else
-  echo "No"
-fi
+# if [[ $str == $CONTRIBUTOR ]]; then
+#   echo "Trigger"
+# else
+#   echo "No"
+# fi
+
+
+PUBLIC_KEY_JSON=$(gh api \
+  -H "Accept: application/vnd.github+json" \
+  /repos/lucetre/beautify-github-contribution/actions/secrets/public-key)
+
+PUBLIC_KEY_ID=$(jq '.key_id' <<<$PUBLIC_KEY_JSON | tr -d '"')
+PUBLIC_KEY=$(jq '.key' <<<$PUBLIC_KEY_JSON)
+ENCRYPTED_SECRET=`python3 tools/encrypt_github_secret.py $PUBLIC_KEY "hello"`
+
+gh api \
+  --method PUT \
+  -H "Accept: application/vnd.github+json" \
+  /repos/lucetre/beautify-github-contribution/actions/secrets/TEST_SECRET \
+  -f encrypted_value="$ENCRYPTED_SECRET" \
+  -f key_id=$PUBLIC_KEY_ID | jq
+
+gh api \
+  -H "Accept: application/vnd.github+json" \
+  /repos/lucetre/beautify-github-contribution/actions/secrets | jq
